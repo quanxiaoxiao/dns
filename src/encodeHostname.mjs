5502114adb53a0abceb42260e20ddb6342eae0bf
quan <quan_xiaoxiao@qq.com>
2024-05-08 11:49:40 +0800

    xxx

--- src/encodeHostname.mjs
+++ src/encodeHostname.mjs
@@ -1,6 +1,8 @@
 import assert from 'node:assert';
 import { enpack } from '@quanxiaoxiao/bytes';
 
+const CHUNK_END = Buffer.from([0x00]);
+
 export default (str) => {
   assert(typeof str === 'string');
   const hostname = str.trim();
@@ -10,6 +12,6 @@ export default (str) => {
   for (let i = 0; i < nameList.length; i++) {
     bufList.push(enpack(nameList[i], 1));
   }
-  bufList.push(Buffer.from([0x00]));
+  bufList.push(CHUNK_END);
   return Buffer.concat(bufList);
 };
