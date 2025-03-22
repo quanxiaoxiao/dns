76616388c49f527d63540fa36d762f77c8f42a44
quan <quan_xiaoxiao@qq.com>
2025-03-22 17:10:02 +0800

    xxx

--- src/decodeHostname.mjs
+++ src/decodeHostname.mjs
@@ -9,9 +9,7 @@ const decodeHostname = (chunk, buf) => {
   const nameList = [];
   let nameSize = chunk.readUint8(0);
   let offset = 1;
-  while (nameSize !== 0
-    && chunk.length > offset
-  ) {
+  while (nameSize !== 0 && chunk.length > offset) {
     if (nameSize === 0xc0) {
       const skip = chunk.readUint16BE(offset - 1) - 0xc000;
       assert(skip <= chunk.length);
--- src/encodeHostname.mjs
+++ src/encodeHostname.mjs
@@ -5,9 +5,9 @@ import { enpack } from '@quanxiaoxiao/bytes';
 const CHUNK_END = Buffer.from([0x00]);
 
 export default (str) => {
-  assert(typeof str === 'string');
+  assert(typeof str === 'string', 'Input must be a string');
   const hostname = str.trim();
-  assert(hostname !== '');
+  assert.strictEqual(hostname !== '', true, 'Hostname cannot be empty');
   const bufList = [];
   const nameList = hostname.split('.');
   for (let i = 0; i < nameList.length; i++) {
