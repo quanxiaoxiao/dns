4647a78144387ac3a0f36724e35e5ab56f49758a
quan <quan_xiaoxiao@qq.com>
2025-03-22 17:33:21 +0800

    xxx

--- src/encode.mjs
+++ src/encode.mjs
@@ -5,16 +5,14 @@ import encodeHostname from './encodeHostname.mjs';
 import { OPCODE_QUERY } from './opcodes.mjs';
 import { RCODE_NOERROR } from './rcodes.mjs';
 
-const QUERY_FLAG = 0;
-// const RESPONSE_FLAG = 1 << 15;
-const TRUNCATE_FLAG_FALSE = 0;
-const AUTHORITATIVE_ANSWER_FALSE = 0;
-// const AUTHORITATIVE_ANSWER_TRUE = 1 << 2;
-// const TRUNCATE_FLAG_TRUE = 1 << 1;
-const RECURSION_DESIRED_TRUE = 1;
-// const RECURSION_DESIRED_FALSE = 0;
-const RECURSION_AVAILABLE_FALSE = 0;
-// const RECURSION_AVAILABLE_TRUE = 1;
+const FLAGS = {
+  QUERY: 0,
+  OPCODE_QUERY: OPCODE_QUERY << 3,
+  AUTHORITATIVE_ANSWER_FALSE: 0 << 2,
+  TRUNCATE_FLAG_FALSE: 0 << 1,
+  RECURSION_DESIRED_TRUE: 1,
+  RECURSION_AVAILABLE_FALSE: 0 << 7,
+};
 
 export default ({
   hostname,
@@ -41,33 +39,29 @@ export default ({
     additionalRecordCountBuf,
   ] = bufList;
 
-  const recordTypeBuf = Buffer.allocUnsafe(2);
-  const classBuf = Buffer.allocUnsafe(2);
-
   identificationBuf.writeUInt16BE(identification);
-  let controlFirst = 0;
-  let controlSecond = 0;
-  controlFirst += QUERY_FLAG;
-  controlFirst += (OPCODE_QUERY << 3);
-  controlFirst += (AUTHORITATIVE_ANSWER_FALSE << 2);
-  controlFirst += (TRUNCATE_FLAG_FALSE << 1);
-  controlFirst += RECURSION_DESIRED_TRUE;
+  const controlFirst = FLAGS.QUERY + FLAGS.OPCODE_QUERY + FLAGS.AUTHORITATIVE_ANSWER_FALSE + FLAGS.TRUNCATE_FLAG_FALSE + FLAGS.RECURSION_DESIRED_TRUE;
   controlFirstBuf.writeUInt8(controlFirst);
 
-  controlSecond += (RECURSION_AVAILABLE_FALSE << 7);
-  controlSecond += RCODE_NOERROR;
+  const controlSecond = FLAGS.RECURSION_AVAILABLE_FALSE + RCODE_NOERROR;
   controlSecondBuf.writeUInt8(controlSecond);
 
   questionCountBuf.writeUInt16BE(1);
   answerRecordCountBuf.writeUInt16BE(0);
   authorityRecordCountBuf.writeUInt16BE(0);
   additionalRecordCountBuf.writeUInt16BE(0);
+
+  const recordTypeBuf = Buffer.allocUnsafe(2);
   recordTypeBuf.writeUInt16BE(recordType);
+
+  const classBuf = Buffer.allocUnsafe(2);
   classBuf.writeUInt16BE(CLASS_IN);
 
-  bufList.push(encodeHostname(hostname));
-  bufList.push(recordTypeBuf);
-  bufList.push(classBuf);
+  bufList.push(
+    encodeHostname(hostname),
+    recordTypeBuf,
+    classBuf,
+  );
 
   return Buffer.concat(bufList);
 };
